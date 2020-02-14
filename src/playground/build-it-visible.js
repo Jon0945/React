51462b5c903b91575574e6c7ac6ee1cbd6d8756f
jon0945 <john.gullne@gmail.com>
2020-02-14 15:17:10 +0100

    Build It 2

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,24 +1,31 @@
-const app = {
-  title: "Visibility Toggle"
-};
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    };
+  }
+  handleToggleVisibility() {
+    this.setState(prevState => {
+      return {
+        visibility: !prevState.visibility
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility == false ? "Show Details" : "Hide Details"}
+        </button>
+        {this.state.visibility == true && (
+          <p>Hey! This is some information you can now see</p>
+        )}
+      </div>
+    );
+  }
+}
+ReactDOM.render(<VisibilityToggle />, document.getElementById("app"));
 
-const appRoot = document.getElementById("app");
-let buttonClicked = false;
-const toggle = () => {
-  buttonClicked = !buttonClicked;
-  render();
-};
-
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      <button onClick={toggle}>
-        {!buttonClicked ? "Show Details" : "Hide Details"}
-      </button>
-      {buttonClicked && <p>Hey! This is some information you can now see</p>}
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-};
-render();
